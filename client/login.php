59cf45832a5f72dbc39149263fdc00a54ed46ae4
llenny18 <ramosalleneid01@gmail.com>
2023-09-28 10:23:24 +0800

    loader

--- client/login.php
+++ client/login.php
@@ -19,7 +19,7 @@ if (isset($_SESSION['userID'])){
         <link rel="stylesheet" href="css/login.css" />
         <link rel="icon" href="images/car_logo.jpg" type="image/x-icon" />
         <script src="js/login.js"></script>
-        
+ 
     </head>
     <body>
     <div class="page">
@@ -48,5 +48,25 @@ if (isset($_SESSION['userID'])){
     </div>
   </div>
 </div>
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+<script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+  <script src="js/alerts.js"></script>
     </body>
 </html>
